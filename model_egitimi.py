3f8ac8a8feefc30b5fefaac1d160a1cf45107773
Akif-Ertas <polseyro@gmail.com>
2026-06-03 08:24:27 +0300

    model egitimi aciklamalari yapildi

--- model_egitimi.py
+++ model_egitimi.py
@@ -1,28 +1,27 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-# confusion_matrix kütüphanesini de ekledik
-from sklearn.metrics import accuracy_score, confusion_matrix
-import joblib
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier #karar agaci (decision tree) 
+from sklearn.model_selection import train_test_split #egitim ve test olarak boler
+from sklearn.metrics import accuracy_score, confusion_matrix #makine ogrenmesi (hata orani hesaplama)
+import joblib #egittigimiz modeli kaydetmek icin kullaniyoruz.
 
-# 1. Veri Setini Oku
+# trafik_verisini okur
 df = pd.read_csv("trafik_verisi.csv")
 
-# 2. Girdileri (X) ve Çıktıyı (y) Ayır
+# girdileri ayirir
 X = df[["saat", "gun", "hava"]]
 y = df["etiket"]
 
-# 3. Veriyi Eğitim ve Test Olarak Böl (%80 Eğitim, %20 Test)
+# egitim ve test verisi olarak boluyoruz(1500 satirlik verinin 8/10u egitim icin 2/10unu test icin veriyoruz)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 4. Modeli Oluştur ve Eğit
+# hangi sartlarda trafik ne oluyor makine onu ogreniyor
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
-# 5. Modelin Başarısını Test Et
+# test verisi ile tahmin yapiyoruz
 tahminler = model.predict(X_test)
 
-# --- RAPOR İÇİN PERFORMANS ÇIKTILARI (ACCURACY, HATA ORANI VE CONFUSION MATRIX) ---
+# yapilan tahminin hata oranini hesapliyoruz
 basari_orani = accuracy_score(y_test, tahminler)
 hata_orani = 1.0 - basari_orani
 
@@ -31,12 +30,11 @@ print(f"Modelin Başarı Oranı (Accuracy): %{basari_orani * 100:.2f}")
 print(f"Modelin Hata Oranı (Error Rate): %{hata_orani * 100:.2f}")
 print("-" * 40)
 
-# Karmaşıklık Matrisi (Confusion Matrix)
+# karmasiklik matrisini hesaplayarak modelin hangi durumlarda ne kadar dogru tahmin yaptigini gosteriyoruz
 cm = confusion_matrix(y_test, tahminler)
 print("Karmaşıklık Matrisi (Confusion Matrix):")
 print(cm)
 print("-" * 40)
-# ----------------------------------------------------------------------------------
 
-# 6. Eğitilmiş Modeli Kaydet
+# her seferinde bastan egitmemek icin model.pkl dosyasina kaydediyoruz
 joblib.dump(model, "model.pkl")
